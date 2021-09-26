c43637f3664d7defe5cc24f6c65b058192aea65b
hongweifang <fhw8234@gmail.com>
2021-09-26 16:32:49 +0800

    Add scripts & update README.md

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # mac setup 
-mainly for developer, it will help to install [oh my zsh](https://github.com/ohmyzsh/ohmyzsh) and [home brew](https://brew.sh/)
+It will install [oh my zsh](https://github.com/ohmyzsh/ohmyzsh) and [home brew](https://brew.sh/)
 
+## Terminal
+suggest to use [iterm2](https://iterm2.com/downloads.html)
+## Steps
+1. install ohmyzsh ```ohmyzsh.sh```
+2. restart terminal 
+3. install homebrew ```homebrew.sh```
+4. install others ```run.sh```
+5. restart terminalk
--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
-echo "installing oh my zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "installed oh my zsh"
+workspace=~/workspace
+if test -d "$workspace"; then
+  echo "folder is existing"
+else
+  echo "creating workspace folder in home(~) folder"
+  mkdir ~/workspace
+  echo "created workspace folder in home folder"
+fi
 
-echo "installing home brew"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "installed home brew"
 
-echo "creating workspace folder in home(~) folder"
-mkdir ~/workspace
-echo "created workspace folder in home folder"
+personal_config_file=~/.personal_config
+if test -f "$personal_config_file"; then
+  echo "file is existing"
+else
+  cp config/.personal_config ~/.personal_config
+fi
